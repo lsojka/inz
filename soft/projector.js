5545512b59fb65c349210807c94a4007722ca824
lsojka <zoltygawron@gmail.com>
2013-12-17 22:26:08 +0100

    uporządkowane

--- soft/projector.js
+++ soft/projector.js
@@ -1,4 +1,6 @@
-// WebGL & THREE.js projector
+/* Viewer - WebGL & THREE.js projector
+ *
+ */
 
         var renderer = null;
 	var scene = null;
@@ -6,6 +8,7 @@
 	var mesh = null;
         // directional lights
         var dlights = new Array();
+        var models = new Array();
         
 Viewer = function()
 {
@@ -18,7 +21,7 @@ Viewer.prototype.init = function(param)
     // "super"constructor ustawiający scenę
     Sim.App.prototype.init.call(this, param);
     this.createCameraControls();
-    
+
     // "latarka" przy kamerze
     dlights[0] = new THREE.DirectionalLight( 0xffffff, 1 /*0.8*/ );
     dlights[0].position.set(-20, 0, 0);
@@ -30,36 +33,25 @@ Viewer.prototype.init = function(param)
 
     var amb = new THREE.AmbientLight( 0x808080, 1);
     this.scene.add(amb);
+    
 }
-/*
-Viewer.prototype.spheresFromSurfacePoints = function(sPoints) // surfacePoints
+
+Viewer.prototype.CreateModelThenAddToScene = function(geometry)
 {
-    for(var i = 0; i < sPoints.length; i++)
-    {
-        console.log("SurfacePoint "+i+" = x "+sPoints[i].x+", y "+sPoints[i].y+", z "+sPoints[i].z);
-        var model = new Pocket();
-        model.init(sPoints[i].x, sPoints[i].y, sPoints[i].z);
-        this.addObject(model);
-    }
-}
-*/
-Viewer.prototype.trianglesToScreen = function(sPoints)
-{
-    var geometry = new THREE.Geometry();
- 
-    for(var i = 0; i < sPoints.length; i++)
-        geometry.vertices.push(new THREE.Vertex(new THREE.Vector3(sPoints[i].x, sPoints[i].y, sPoints[i].z)));
-    
-    var triangles = TrianglesFromSurfacePoints(sPoints, 20);
-    
-    // face = polygon = składa się z surfacePointów przypisanych do trójkąta
-    for(var j = 0; j < (triangles.length); j++)
-        geometry.faces.push(new THREE.Face3(triangles[j].verticesNumerals[0],triangles[j].verticesNumerals[1],triangles[j].verticesNumerals[2]));
-    
-    geometry.computeFaceNormals();
-    var model = new Pocket();
+    // TODO : viewer tylko doczytuje gotowe, niezaleznie tworzone
+    model = new Model();
+    models.push(model);
     model.init(geometry, false);
     this.addObject(model);
+    console.log("Mesh created & added to scene.");
+    //console.log("Models size :" + models.length+", contains "+models);
+}
+
+Viewer.prototype.createLocalCube = function()
+{
+    var cube = new GlobalCube();
+    cube.init();
+    this.addObject(cube);
 }
 
 // trackball
@@ -81,8 +73,9 @@ Viewer.prototype.createCameraControls = function()
     
     this.controls = controls;
     
-
+    this.camera.position.z = 3000;
 }
+
 Viewer.prototype.update = function()
 {
     this.controls.update();
@@ -90,36 +83,12 @@ Viewer.prototype.update = function()
     // przesuń latarkę do położenia kamery
     dlights[0].position = camera.position;
 }
-
-Viewer.CAMERA_START_Z = 30;
-Viewer.CAMERA_RADIUS = 20;
+// parametry na sztywno, zmodyfikować
+Viewer.CAMERA_START_Z = 3000;
+Viewer.CAMERA_RADIUS = 2000;
 Viewer.MIN_DISTANCE_FACTOR = 1.1;
-Viewer.MAX_DISTANCE_FACTOR = 20;
+Viewer.MAX_DISTANCE_FACTOR = 2000;
 Viewer.ROTATE_SPEED = 1.0;
 Viewer.ZOOM_SPEED = 3;
 Viewer.PAN_SPEED = 0.2;
 Viewer.DAMPING_FACTORS = 0.3;
-
-Pocket = function()
-{
-	Sim.Object.call(this);
-}
-Pocket.prototype = new Sim.Object();
-Pocket.prototype.init = function(geometry, if_wireframe)
-{
-    var material = new THREE.MeshPhongMaterial();
-    material.wireframe = if_wireframe;
-    material.color = new THREE.Color("0x00ffff");
-    
-    var mesh = new THREE.Mesh( geometry, material ); 
-    mesh.position = new THREE.Vector3(0,0,0); // !
-    this.setObject3D(mesh);   
-    
-    dlights[0].target = this;
-}
-
-Pocket.prototype.update = function()
-{
-    //this.object3D.rotation.x += 0.0025;
-    //this.object3D.rotation.y += 0.0025;   
-}
