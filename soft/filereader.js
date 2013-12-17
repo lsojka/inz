f406a72b616f9c8a411e0a8e13cb4ac79ee4e469
lsojka <zoltygawron@gmail.com>
2013-12-17 22:25:19 +0100

    fix wczytywania plikow roznych wielkosci

--- soft/filereader.js
+++ soft/filereader.js
@@ -11,21 +11,29 @@ function extractFile(file, onLoadCallback)
 
 function fileDataToControlPoints(str)
 {
-    var cPoints; // 2D array TODO: 1D
-    var fnSize = 8; //fixed nurbs size; liczba punktów w pliku
+    var cPoints = new Array(); // 2D array
+    var fRows; //file Rows
+    var fCols; //file Cols
 
-    cPoints = new Array();
-    for(var ii = 0; ii<fnSize; ii++)
+    var lineTemp = str.split("\n");
+    fRows = lineTemp.length;
+    pointStrings = str.split(";");
+    
+    if( pointStrings[pointStrings.length - 1].length == 0 )
+        pointStrings.pop();
+    
+    fCols = pointStrings.length / fRows;
+    //console.log("Points = "+fRows+","+fCols);
+   
+    for(var ii = 0; ii<fRows; ii++)
     {
             cPoints[ii] = new Array();
-            for(var jj = 0; jj<fnSize; jj++)
+            for(var jj = 0; jj<fCols; jj++)
             {
                 cPoints[ii][jj] = new Point();
             }
     }
-    
-    pointStrings = str.split(";");
-    // dla każdego substringa punktu (jego współrzędzne)
+    // -------------------------------------------------------------------------
     for(var i = 0; i < pointStrings.length; i++)
     {  
         pointStringsCoords = pointStrings[i].split(" ");
@@ -35,15 +43,17 @@ function fileDataToControlPoints(str)
         for(var j = 0; j < pointStringsCoords.length; j++)
         {
             var temp = parseFloat(pointStringsCoords[j]);
-            // odfiltrowanie substringow nie bedacych liczbami
             if( (temp < 0) || (temp == 0) || (temp > 0) )
             {
-                    inputXYZ[itoken] = temp;
-                    itoken = itoken + 1;
+                inputXYZ[itoken] = temp;
+                itoken = itoken + 1;
             }
         }
         if(inputXYZ.length > 0/*== 3*/)
-            cPoints[(i-(i%fnSize))/fnSize][i%fnSize].setXYZ(inputXYZ[0], inputXYZ[1], inputXYZ[2]);
+        {          
+            cPoints[(i-(i%fCols))/fCols][i%fCols].setXYZ(inputXYZ[0], inputXYZ[1], inputXYZ[2]);
+            //console.log("PS "+i+" cPoint ["+(i-(i%fCols))/fCols+"]["+i%fCols+"] = "+cPoints[(i-(i%fCols))/fCols][i%fCols].x+","+cPoints[(i-(i%fCols))/fCols][i%fCols].y+","+cPoints[(i-(i%fCols))/fCols][i%fCols].z);
+        }
     }
     return cPoints;
 }
